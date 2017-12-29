04c74d77b146ecf89940e8c0d191e2dec0293666
Marcus <marcusaclayton@gmail.com>
2017-12-29 02:45:27 -0800

    Updated style sheet, index, and main.js

--- BACKUP/assets/js/main.js
+++ BACKUP/assets/js/main.js
@@ -10,9 +10,3 @@ var form = {
 xhr.send(JSON.stringify(form));
 return false;
 });
-
-function initializeLinks() {
-    $('.logout').each(function () {
-      $(this).prop('href', '/.auth/logout?post_logout_redirect_uri=' + encodeURI(window.location.pathname));
-    });
-  }
