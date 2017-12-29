0af187c0803505cd1254f89e44be116da24c90f4
Marcus <marcusaclayton@gmail.com>
2017-12-28 19:17:33 -0800

    Adding test files for ajax call method

---
+++ assets/js/test.js
@@ -0,0 +1,21 @@
+document.getElementById('customerForm').addEventListener('submit', function (e) {
+    var functionUrl = "https://mc-test-function-004.azurewebsites.net/api/newuser";
+    var form = {
+        customerName: document.getElementById("customerName").value
+    };
+    $.ajax({
+        method: "POST",
+        url: functionUrl,
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        data: JSON.stringify(form),
+        success: function(data){
+            alert('Request submitted');
+        },
+        error: function(data){
+            alert('Whoops, something went wrong.')
+            console.info(data);
+        }
+    });
+});
